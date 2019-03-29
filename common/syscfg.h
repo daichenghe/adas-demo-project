dfaf18bfe42b5247ae8619f02b80ea38aa6cba48
daichenghe <41727862+geqian@users.noreply.github.com>
2019-03-29 09:09:57 +0800

    Add files via upload

---
+++ common/syscfg.h
@@ -0,0 +1,15 @@
+﻿#ifndef _SYSCFG_H_
+#define _SYSCFG_H_
+
+float read_conf_value(const char *key,  FILE *fp);
+int write_conf_value(const char *key, float value, FILE *fp);
+
+FILE * read_conf_pre(const char *file);
+int read_conf_ok(FILE *fp);
+
+int write_conf_ok(FILE *fp);
+FILE * write_conf_pre(const char *file);
+
+
+#endif //end of FILE_H_
+
